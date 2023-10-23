c0c1d7f9bb36ce34b01e607ce8c61f9347f0fcc4
Alex <alexeusgr@gmail.com>
2023-10-23 19:41:17 +0300

    add recursive line

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import turtle as t
 
-t.forward(100)
+# function with a parameter which has default value, 
+def f(i = 1):
+    print(i)
 
+def draw_line(l = 100):
+    t.forward(l)
+# recursive line:
+def recursive_line(lenght = 100, heading = 0):
+    t.forward(lenght) 
+    t.setheading(heading)
+    if lenght < 1:
+        return input()
+    else:  
+        return recursive_line(lenght - 10, heading + 10)
+
+recursive_line()
+# line()
+# f(5)
 input()
